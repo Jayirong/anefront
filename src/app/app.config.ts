859a9fb45f0a23d1900fb9d9e9ae766a19f78b54
Jayirong <lemonsoft5555@gmail.com>
2025-01-13 04:57:16 -0300

    funciona :)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,13 +8,17 @@ import { MSAL_INSTANCE, MsalService } from '@azure/msal-angular';
 
 //vamo a ver si funciona la config de msal
 export function MSALFactory(): IPublicClientApplication {
-  return new PublicClientApplication({
+  const msalInstance = new PublicClientApplication({
     auth: {
       clientId: '1ec53a08-275a-45d7-9e68-d5e97ec990be',
       authority: 'https://login.microsoftonline.com/7d5be4db-7c52-4508-b95f-b8e8d3b2b3ec',
       redirectUri: 'http://localhost:4200'
     }
   });
+
+  msalInstance.initialize();
+
+  return msalInstance;
 
 }
 
